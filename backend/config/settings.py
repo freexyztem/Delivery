7f2c788336a6c532765a46175e960a810d1b2a05
Alexi Gonzalez <freexyztem@gmail.com>
2026-09-07 23:48:58 -0400

    adjust setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -89,14 +89,6 @@ DATABASES = {
         conn_max_age=600,
     )
 }
-"""
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
